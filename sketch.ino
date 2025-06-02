ebb8c25a1031542fa4acb87771c183dae82b26d1
V.S.BHAVIN KRISHNA <vsbhavinkrishna@gmail.com>
2025-06-02 23:06:32 +0530

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+}
+
+void loop() {
+  Serial.println("I am");
+  delay(500);
+  Serial.println("V.S. BHAVIN");
+  delay(500);
+  Serial.println("KRISHNA");
+  delay(500);
+}
