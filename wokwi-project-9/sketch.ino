fc58281ffbb675b4b3e03dce36f73f141be1f0e2
V.S.BHAVIN KRISHNA <vsbhavinkrishna@gmail.com>
2025-06-04 09:53:52 +0530

    Update sketch.ino

--- wokwi-project-9/sketch.ino
+++ wokwi-project-9/sketch.ino
@@ -1,25 +1,11 @@
-const int buzzer=4;
-const int ldr=34;
-void setup() 
-{
+void setup() {
   Serial.begin(115200);
-  pinMode(ldr, INPUT);
-  pinMode(buzzer, OUTPUT);
+
 }
 
-void loop() 
-{
- int a=analogRead(ldr);
- Serial.println(a);
- delay(500);
- if (a>=1500)
-
-  {
-  tone(buzzer,400);
-  }
-  else
-  {
-    noTone(buzzer);
-  }
- 
-} 
+void loop() {
+    int a = analogRead(34);
+    Serial.println(a);
+    delay(500);
+  
+}
