73b7abe37634a4743e4a68cdf5605532bf2a629a
V.S.BHAVIN KRISHNA <vsbhavinkrishna@gmail.com>
2025-06-04 09:49:26 +0530

    Add files via upload

--- wokwi-project-9/sketch.ino
+++ wokwi-project-9/sketch.ino
@@ -1,11 +1,25 @@
-void setup() {
+const int buzzer=4;
+const int ldr=34;
+void setup() 
+{
   Serial.begin(115200);
-
+  pinMode(ldr, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
-void loop() {
-    int a = analogRead(34);
-    Serial.println(a);
-    delay(500);
-  
-}
+void loop() 
+{
+ int a=analogRead(ldr);
+ Serial.println(a);
+ delay(500);
+ if (a>=1500)
+
+  {
+  tone(buzzer,400);
+  }
+  else
+  {
+    noTone(buzzer);
+  }
+ 
+} 
